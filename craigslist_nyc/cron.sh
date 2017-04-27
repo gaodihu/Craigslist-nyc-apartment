fac37596b1787e4f0cfd914d1af65265ad395a07
zhehaowang <wangzhehao410305@gmail.com>
2017-04-27 11:27:26 -0700

    cron: add cron for periodic execution of spider

---
+++ craigslist_nyc/cron.sh
@@ -0,0 +1,4 @@
+echo "one more time!"
+export PYTHONPATH=/Library/Python/2.7/site-packages:$PYTHONPATH
+cd /Users/zhehaowang/projects/others/Craigslist-nyc-apartment/craigslist_nyc
+/Users/zhehaowang/Library/Python/2.7/bin/scrapy crawl craig
