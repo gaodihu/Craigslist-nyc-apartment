f27ec06ab47e8e8a2634dae4c74b3c5d73d93179
zhehaowang <wangzhehao410305@gmail.com>
2017-06-05 10:24:50 -0400

    emailer: remove personal credential

--- craigslist_nyc/emailer/emailer.py
+++ craigslist_nyc/emailer/emailer.py
@@ -16,30 +16,22 @@ def send_email():
     print url
     msg = Message(
             'Young professional interested in your apartment!',
-            sender = 'testname.zhehao@gmail.com',
-            reply_to = 'Zhehao Wang <zhehao@cs.ucla.edu>',
+            sender = 'your.name@gmail.com',
+            reply_to = 'Your Other Email <your.prefered.email@gmail.com>',
             recipients = [recipient])
     msg.body = """
 Hi there!
 
-This is Zhehao. I'm planning to move to NYC in mid June, and saw your post (""" + url + """) on Craigslist. 
+This is your.name. I'm planning to move to NYC in mid June, and saw your post (""" + url + """) on Craigslist. 
 I'm very interested in learning more!
 
-Something about myself:
- - I'm going to work as a software engineer at Bloomberg starting 06/19/17 (ideally looking to move in around that date but flexible)
- - I'm a clean, easy going, and respectful guy turning 24 soon. No habit of partying and won't have guests staying over night.
- - I'm currently living in Los Angeles, and just finished my masters program in Computer Science at UCLA. I'm originally from China but have been living in the US for 4 years.
- - I've a good credit record (720+), and can show proof of income (company offer letter) if required.
- - My LinkedIn profile (https://www.linkedin.com/in/zhehao-wang-15739869/).
- - I'll be out of the apartment working most of the time between 9A-8P on weekdays.
-
 If the apartment's still available and I might be a good fit, would it be possible to do a FaceTime / Skype call and go over more details?
-My email address is zhehao@cs.ucla.edu, and phone number is 424-333-8516.
+My email address is your.name@gmail.com, and phone number is your.phone.number.
 
 Thank you very much and please let me know!
 
 Thanks,
-Zhehao
+your.name
 """
     mail.send(msg)
     return "Sent"
--- craigslist_nyc/emailer/settings.py
+++ craigslist_nyc/emailer/settings.py
@@ -1,10 +1,10 @@
 URL = "http://localhost:5000"
 
-MAIL_FROM = "testname.zhehao@gmail.com"
+MAIL_FROM = "your.username@gmail.com"
 MAIL_SERVER = "smtp.gmail.com"
 
 MAIL_PORT = 465
-MAIL_USERNAME = "testname.zhehao@gmail.com"
-MAIL_PASSWORD = "test@2015"
+MAIL_USERNAME = "your.username@gmail.com"
+MAIL_PASSWORD = "your.password"
 MAIL_USE_SSL = True
 
